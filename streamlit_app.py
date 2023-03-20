4cf6d14102ed01784f1e3a6da61049648a8ed6c6
presklis <54169311+presklis@users.noreply.github.com>
2023-03-20 22:12:17 +0000

    Update streamlit_app.py
    added title

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+streamlit.title('My Parents New Healty Dinner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
