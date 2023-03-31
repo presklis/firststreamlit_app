d28248ba4750ebac54802a53616e2da67e2b5c52
presklis <54169311+presklis@users.noreply.github.com>
2023-03-31 14:39:37 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,6 +37,6 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
 my_data_rows = my_cur.fetchall()
-streamlit.header("Fruit List As Follow")
+streamlit.header("Fruit Information")
+add_my_fruit = streamlit.text_input('What fruit would you like to information about?','Banana')
 streamlit.dataframe(my_data_rows)
-add_my_fruit = streamlit.text_input('What fruit would you like to information about?','Banana');
