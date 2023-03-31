8504349b8a493d69df7fdeadc8389c01b2ae1782
presklis <54169311+presklis@users.noreply.github.com>
2023-03-31 14:38:50 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,4 +39,4 @@ my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
 my_data_rows = my_cur.fetchall()
 streamlit.header("Fruit List As Follow")
 streamlit.dataframe(my_data_rows)
-
+add_my_fruit = streamlit.text_input('What fruit would you like to information about?','Banana');
